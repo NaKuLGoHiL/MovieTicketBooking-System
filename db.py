50f889e28e63071ecb0222fe665a95938ef30075
Atharv982 <iyengaratharv@gmail.com>
2026-04-23 18:41:11 +0530

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def connect_db():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Nakul2803",   # ⚠️ CHANGE THIS
-        database="movie_booking"
-    )
